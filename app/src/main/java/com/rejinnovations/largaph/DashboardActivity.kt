a79683972a04298f4d528cbe4ccc87154cd7c451
Raven Mahinay <78337-22213227@SERVER2.DOMAIN>
2024-02-28 10:24:41 +0800

    first commit

---
+++ app/src/main/java/com/rejinnovations/largaph/DashboardActivity.kt
@@ -0,0 +1,17 @@
+package com.rejinnovations.largaph
+
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
